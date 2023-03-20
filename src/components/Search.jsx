e88ca76ea05dd96407b9ff1b36f4d61131739471
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-03-20 18:56:29 +0530

    2 commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { browserHistory as history } from 'react-router';
 
-/*
-This component displays a form where the user can enter a GitHub username
-When they submit the form either by pressing ENTER or clicking the button,
-we will use react-router's history.push function to push a new URL to the history.
 
-This will have as an effect to navigate to a new URL, which will display the User component
-Why are we doing this instead of using a <Link>? The answer is straightforward, but make sure you understand!!!
-*/
 class Search extends React.Component {
     constructor(props) {
         super(props);
 
-        // Why do we need to do this?? Make sure you understand!!!
         this._handleSubmit = this._handleSubmit.bind(this);
     }
     _handleSubmit(e) {
